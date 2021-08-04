0ee2b3beeab148b97224c8cba6bf702a09fa84d9
User <User@DESKTOP-9KIBIEH>
2021-08-04 15:23:24 +1000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace University_Orientation_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
